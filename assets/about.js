e9e8145008d14ba43359c8ed7bcc818b68191b4f
vinod <viond@shoptrade.co>
2022-10-28 13:09:11 +0530

    contact us page reff 4857601 updated

---
+++ assets/about.js
@@ -0,0 +1,93 @@
+
+
+$(document).ready(function(){
+
+    let aboutCulterTop = new Swiper('.js-top-image-slider', {
+
+        // slidesPerView: 3,
+        centerdSlides:"true",
+        loop:true,
+        freemode : true,
+        autoplay: {
+            duration:2000
+          },
+          spaceBetween: 40,
+        breakpoints: {
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1028: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1.3,
+                spaceBetween: 10
+            }
+        }
+      });
+    let aboutCulterBottom = new Swiper('.js-bottom-image-slider', {
+
+        // slidesPerView: 3,
+        centerdSlides:"true",
+        loop:true,
+        freemode : true,
+
+        autoplay: {
+            duration:3000
+        },
+        spaceBetween: 40,
+       
+        // direction :"ltr",
+        breakpoints: {
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1028: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1.3,
+                spaceBetween: 10
+            }
+        }
+      });
+    let aboutTeam = new Swiper('.js-team-slider', {
+
+        slidesPerView: 1,
+       
+        loop:true,
+        freeMode : true,
+
+        // autoplay: {
+        //     duration:3000
+        // },
+        spaceBetween: 40,
+        navigation: {
+          nextEl: ' .js-arrow-right ',
+          prevEl: ' .js-arrow-left',
+        },
+       
+        // direction :"ltr",
+        breakpoints: {
+            1920: {
+                slidesPerView: 1,
+                spaceBetween: 30
+            },
+            1028: {
+                slidesPerView: 1,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1.2,
+                spaceBetween: 10
+            }
+        }
+      });
+
+
+
+})
--- assets/common.js
+++ assets/common.js
@@ -15,4 +15,64 @@ $(document).ready(function(){
     $(".model-popup").fadeOut()
 
   })
+
+
+
+
+  //count on scroll
+
+  var counted = 0;
+  $(window).scroll(function () {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (counted == 0 && $(window).scrollTop() > oTop) {
+      $('.count').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
+          countNum: countTo
+        },
+
+          {
+
+            duration: 500,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+
+              function convertToInternationalCurrencySystem(labelValue) {
+
+                // Nine Zeroes for Billions
+                return Math.abs(Number(labelValue)) >= 1.0e+9
+
+                  ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + "B"
+                  // Six Zeroes for Millions 
+                  : Math.abs(Number(labelValue)) >= 1.0e+6
+
+                    ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + "M"
+                    // Three Zeroes for Thousands
+                    : Math.abs(Number(labelValue)) >= 1.0e+3
+
+                      ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
+
+                      : Math.abs(Number(labelValue));
+
+              }
+
+              let finalCount = convertToInternationalCurrencySystem(this.countNum)
+              $this.text(finalCount + "+");
+              //alert('finished');
+            }
+
+          });
+      });
+      counted = 1;
+    }
+
+  });
+
 })
