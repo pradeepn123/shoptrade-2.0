1b60ff996b1d904ec25ab8208e5957109fd8df3f
Shivanandkkhh <Shivanand@shoptrade.co>
2023-04-20 23:17:29 +0530

    images scroll section bug fixes

--- assets/common.js
+++ assets/common.js
@@ -894,3 +894,30 @@ $('.accordion__header').click(function(e) {
 
 
 
+// Imagies scroll manuel
+const slider = document.querySelector('.grid-item-main-scroll');
+let isDown = false;
+let startX;
+let scrollLeft;
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
