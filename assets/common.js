a4cd59698082aee811c0e8e9187f8012ceb93569
Vasavi <nagavasavi@shoptrade.co>
2023-05-05 17:34:46 +0530

    audit report page

--- assets/common.js
+++ assets/common.js
@@ -1062,6 +1062,19 @@ var swiper_manual = new Swiper('.grid-item-main-scroll', {
   }
 });
 
+//Audit page - FAQ Accordion//
+$('.accordion__header').click(function(e) {
+	e.preventDefault();
+	var currentIsActive = $(this).hasClass('is-active');
+	$(this).parent('.accordion').find('> *').removeClass('is-active');
+  $('.accordion__toggle svg').removeClass('rotate_svg');
+	if(currentIsActive != 1) {
+		$(this).addClass('is-active');
+		$(this).next('.accordion__body').addClass('is-active');
+    $(this).find('.accordion__toggle svg').addClass('rotate_svg');
+	}
+});
+
 
 
 
