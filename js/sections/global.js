e23e3aededb527ea1b0654bc7159192b71dac019
ShopTrade S47 <shoptrade47@ShopTrades-MacBook-Pro-4.local>
2024-03-20 12:07:05 +0530

    code added to remote

--- js/sections/global.js
+++ js/sections/global.js
@@ -1,16 +1,3 @@
-//global imports here
-import lazysizes from 'lazysizes';
-import 'lazysizes/plugins/object-fit/ls.object-fit';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import 'lazysizes/plugins/rias/ls.rias';
-import 'lazysizes/plugins/bgset/ls.bgset';
-import 'lazysizes/plugins/respimg/ls.respimg';
-
-import TabbedComponent from "./tabbed-component";
-//lazyload image dependencies
-lazysizes.cfg.loadMode = 1; //init lazyload
-
 
 document.addEventListener('DOMContentLoaded', () => {
-    TabbedComponent();
 })
--- js/sections/tabbed-component.js
+++
@@ -1,6 +0,0 @@
-import svelteWrapper from "JsComponents/svelte-wrapper";
-import TabbedComponent from  "SvelteComponents/tabbed-component.svelte";
-
-export default () => {
-    svelteWrapper(TabbedComponent, 'tabbed-component' , '');
-}
